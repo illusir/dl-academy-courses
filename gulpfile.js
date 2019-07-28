d1f785ae7be50546d8d76a7d3b8304f744d0cd99
illusir <taom.fox@gmail.com>
2019-07-28 15:45:28 +0400

    123

--- gulpfile.js
+++ gulpfile.js
@@ -55,7 +55,7 @@ gulp.task('sass', function () {
     ])
         .pipe(gulpif(ENV != 'production', sourceMaps.init()))
         .pipe(sass.sync().on('error', sass.logError))
-        .pipe(autoprefixer({browsers: ['last 10 versions'], cascade: false}))
+        .pipe(autoprefixer({cascade: false}))
         .pipe(cleanCSS())
         .pipe(concat('./style.css'))
         .pipe(gulpif(ENV != 'production', sourceMaps.write()))
