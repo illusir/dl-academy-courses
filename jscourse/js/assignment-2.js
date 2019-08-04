d1a204534565af3736091a83652cee28cad3d460
illusir <taom.fox@gmail.com>
2019-08-05 02:48:22 +0400

    arrays js assignment

--- jscourse/js/assignment-2.js
+++ jscourse/js/assignment-2.js
@@ -3,42 +3,136 @@
 document.getElementById("button").onclick = function () {
 
   console.log ('==========');
-  console.log ('Assignment #1');
+  console.log ('Assignment #2');
 
-  var num = 5;
-  var str = "five";
-  var bool = true;
+  var numOne = 5;
+  var numTwo = 3;
+  var stringOne = "Five";
+  var stringTwo = "Foxtrot"
+  var stringThree = "5";
+  var bool = false;
   var zero = null;
   var und = undefined;
-  //bonus
-  var obj = {};
 
-  alert ("Type of" + " " + num + " " + "is" + " " + typeof num);
-  alert ("Type of" + " " + str + " " + "is" + " " + typeof str);
-  alert ("Type of" + " " + bool + " " + "is" + " " + typeof bool);
-  alert ("Type of" + " " + zero + " " + "is" + " " + typeof zero);
-  alert ("Type of" + " " + und + " " + "is" + " " + typeof und);
-  alert ("Bonus - Type of" + " " + obj + " " + "is" + " " + typeof obj);
+  if (numOne == numTwo) {
+    alert ("Number" + " " + numOne + " " + "is Equal to" + " " + numTwo);
+    console.log ("Number" + " " + numOne + " " + "is Equal to" + " " + numTwo);
+  } else if (numOne > numTwo) {
+    alert ("Number" + " " + numOne + " " + "is Bigger than" + " " + numTwo);
+    console.log ("Number" + " " + numOne + " " + "is Bigger than" + " " + numTwo);
+  } else {
+    alert ("Number" + " " + numOne + " " + "is Smaller than" + " " + numTwo);
+    console.log ("Number" + " " + numOne + " " + "is Smaller than" + " " + numTwo);
+  }
+
+  if (stringOne == stringTwo) {
+    alert ("Word" + " " + stringOne + " " + "is Equal to" + " " + stringTwo);
+    console.log ("Word" + " " + stringOne + " " + "is Equal to" + " " + stringTwo);
+  } else if (stringOne > stringTwo) {
+    alert ("Word" + " " + stringOne + " " + "is Bigger than" + " " + stringTwo);
+    console.log ("Word" + " " + stringOne + " " + "is Bigger than" + " " + stringTwo);
+  } else {
+    alert ("Word" + " " + stringOne + " " + "is Smaller than" + " " + stringTwo);
+    console.log ("Word" + " " + stringOne + " " + "is Smaller than" + " " + stringTwo);
+  }
+
+  if (zero == und) {
+    alert ("For == Null" + " " + "is Equal to" + " " + "Undefined");
+    console.log ("For == Null" + " " + "is Equal to" + " " + "Undefined");
+  } else {
+    alert ("For == Null" + " " + "is NOT Equal to" + " " + "Undefined");
+    console.log ("For == Null" + " " + "is NOT Equal to" + " " + "Undefined");
+  }
+
+  if (zero === und) {
+    alert ("For === Null" + " " + "is Equal to" + " " + "Undefined");
+    console.log ("For === Null" + " " + "is Equal to" + " " + "Undefined");
+  } else {
+    alert ("For === Null" + " " + "is NOT Equal to" + " " + "Undefined");
+    console.log ("For === Null" + " " + "is NOT Equal to" + " " + "Undefined");
+  }
+
+  if (numOne == stringThree) {
+    alert ("Number" + " " + numOne + " " + "is Equal to String" + " " + stringThree);
+    console.log ("Number" + " " + numOne + " " + "is Equal to String" + " " + stringThree);
+  } else if (numOne > numTwo) {
+    alert ("Number" + " " + numOne + " " + "is Bigger than String" + " " + stringThree);
+    console.log ("Number" + " " + numOne + " " + "is Bigger than String" + " " + stringThree);
+  } else {
+    alert ("Number" + " " + numOne + " " + "is Smaller than String" + " " + stringThree);
+    console.log ("Number" + " " + numOne + " " + "is Smaller than String" + " " + stringThree);
+  }
+
+  if (bool == zero) {
+    alert (bool + " " + "is Equal to" + " " + zero);
+    console.log (bool + " " + "is Equal to" + " " + zero);
+  } else {
+    alert (bool + " " + "is NOT Equal to" + " " + zero);
+    console.log (bool + " " + "is NOT Equal to" + " " + zero);
+  }
 
   alert ("Results Logged into Console");
 
-  console.log ("Type of" + " " + num + " " + "is" + " " + typeof num);
-  console.log ("Type of" + " " + str + " " + "is" + " " + typeof str);
-  console.log ("Type of" + " " + bool + " " + "is" + " " + typeof bool);
-  console.log ("Type of" + " " + zero + " " + "is" + " " + typeof zero);
-  console.log ("Type of" + " " + und + " " + "is" + " " + typeof und);
-  console.log ("Bonus - Type of" + " " + obj + " " + "is" + " " + typeof obj);
-
-}
+  }
 
 document.getElementById("button2").onclick = function () {
 
   console.log ('==========');
-  console.log ('Assignment #1.2');
+  console.log ('Assignment #2.2');
 
-  var num = prompt("Enter a Number");
-  alert ("Your Number" + " " + num + " " + "Squared is" + " " + Math.pow(num, 2)); //found this, decided to try, num*num also works
-  alert ("Results Logged into Console");
-  console.log("Your Number" + " " + num + " " + "Squared is" + " " + Math.pow(num, 2));
+  var numOne = prompt("First Number");
+  var numTwo = prompt("Second Number");
+
+  function compare() {
+      if (numOne == numTwo) {
+      alert ("First Number" + " " + numOne + " " + "is Equal to Second Number" + " " + numTwo);
+      console.log ("First Number" + " " + numOne + " " + "is Equal to Second Number" + " " + numTwo);
+    } else if (numOne > numTwo) {
+      alert ("The Biggest number is the First -" + " " + numOne);
+      console.log ("The Biggest number is the First -" + " " + numOne);
+    } else {
+      alert ("The Biggest number is the Second -" + " " + numTwo);
+      console.log ("The Biggest number is the Second -" + " " + numTwo);
+    }
+  }
+
+  if ((numOne && numTwo != "") && (isNaN(numOne) == false) && (isNaN(numTwo) == false)) {
+    compare();
+    alert ("Results Logged into Console");
+  } else {
+    alert ("One of fields is empty or is not a number. Enter a number");
+    var numOne = prompt("First Number");
+    var numTwo = prompt("Second Number");
+    compare();
+  }
+
+}
+
+//not validated for numbers!
+document.getElementById("button3").onclick = function () {
+
+  console.log ('==========');
+  console.log ('Assignment #2.3');
+
+  var letterOne = prompt("First Letter");
+  var letterTwo = prompt("Second Letter");
+
+  letterOne = letterOne.toLowerCase();
+  letterTwo = letterTwo.toLowerCase();
+
+  function compare() {
+      if (letterOne == letterTwo) {
+      alert ("First Letter" + " " + letterOne + " " + "is Equal to Second Letter" + " " + letterTwo);
+      console.log ("First Letter" + " " + letterOne + " " + "is Equal to Second Letter" + " " + letterTwo);
+    } else if (letterOne < letterTwo) {
+      alert ("The Former Letter is -" + " " + letterOne);
+      console.log ("The Former Letter is -" + " " + letterOne);
+    } else {
+      alert ("The Former Letter is -" + " " + letterTwo);
+      console.log ("The Former Letter is -" + " " + letterTwo);
+    }
+  }
+
+  compare();
 
 }
---
+++ jscourse/js/assignment-3.js
@@ -0,0 +1,79 @@
+"use strict";
+
+document.getElementById("button").onclick = function () {
+
+  console.log ('==========');
+  console.log ('Assignment #3');
+
+  var userName = prompt("Enter Name");
+  var userAge = prompt("Enter Age");
+  var user = {
+    name: userName,
+    age: userAge,
+  };
+
+  console.log (user);
+  alert ("Results Logged into Console");
+
+}
+
+document.getElementById("button2").onclick = function () {
+
+  console.log ('==========');
+  console.log ('Assignment #3.2');
+
+  var arr = [];
+  var num = prompt("While Method - Enter a Value");
+
+  //while
+
+  function arrayWhile() {
+    while (arr.length <= 4) {
+      if (arr.length < 4) {
+        arr.push(num);
+        var num = prompt("While Method - Enter a Value");
+      } else {
+        arr.push(num);
+      }
+    }
+  }
+
+  if (num == "") {
+    alert("Enter a Value First");
+    return false;
+    } else {
+    arrayWhile();
+    }
+
+  console.log ("While Method Array is:" + " " + arr.reverse());
+  alert ("Results Logged into Console");
+  arr.length = 0;
+
+//for
+
+for (var i = 0; i <= 4; i++) {
+  var num = prompt("For Method - Enter a Value");
+  arr.push(num);
+}
+
+console.log ("For Method Array is:" + " " + arr.reverse());
+alert ("Results Logged into Console");
+
+}
+
+document.getElementById("button3").onclick = function () {
+
+  console.log ('==========');
+  console.log ('Assignment #3');
+
+  var userName = prompt("Enter Name");
+  var userAge = prompt("Enter Age");
+  var user = {
+    name: userName,
+    age: userAge,
+  };
+
+  console.log (user);
+  alert ("Results Logged into Console");
+
+}
